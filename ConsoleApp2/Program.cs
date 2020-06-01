ff1d877bf818baa73ffe35c93c4815e42623d33b
Robert Stefaniak <01138659@pw.edu.pl>
2020-06-01 18:39:09 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////int liczba;
+            //char znak;
+            ////liczba = int.Parse(Console.ReadLine());
+            //znak = char.Parse(Console.ReadLine());
+            //switch(znak)
+            //{
+            //    case 'a': Console.WriteLine("A");
+            //        break;
+            //    case 'b': Console.WriteLine("B");
+            //        break;
+            //    case 'c': Console.WriteLine("C");
+            //        break;
+            //    default: Console.WriteLine("Inna wartosc.");
+            //        break;
+            //}
+            int[] tablica = new int[4];
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                tablica[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                Console.Write(tablica[i]+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
